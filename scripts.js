ccf21d500193f482c970abc9eddafe781114ee6e
Matt Eilar and Winter Runion <winter@>
2019-05-15 20:43:34 -0700

    Complete calculator backend.

--- scripts.js
+++ scripts.js
@@ -3,5 +3,45 @@ var add = function(num1, num2) {
 };
 var num1 = parseInt(prompt("Enter a number:"));
 var num2 = parseInt(prompt("Enter another number:"));
+var result = add(num1, num2);
+alert(result);
 
-alert(add(num1, num2));
+var subtract = function(num1, num2) {
+  return num1 - num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = subtract(num1, num2);
+alert(result);
+
+var divide = function(num1, num2) {
+  return num1 / num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = divide(num1, num2);
+alert(result);
+
+var remainder = function(num1, num2) {
+  return num1 % num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = remainder(num1, num2);
+alert(result);
+
+var multiply = function(num1, num2) {
+  return num1 * num2;
+};
+var num1 = parseInt(prompt("Enter a number:"));
+var num2 = parseInt(prompt("Enter another number:"));
+var result = multiply(num1, num2);
+alert(result);
+
+
+
+
+
+
+
+//alert(add(num1, num2));
