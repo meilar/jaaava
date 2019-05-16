f10028aa96a7de8feac144094162811b3bde8368
Matt Eilar and Winter Runion <winter@>
2019-05-15 20:14:22 -0700

    Add super spiffy quest info.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//var userName = prompt("Hello! What is your name?");
+//alert("Welcome, " + userName);
+
+var userAge = prompt('What is your age?');
+var userName = prompt('What is your name?');
+var userQuest = prompt('What is your quest?');
+
+alert('Welcome ' + userName + "!" + " We love people who are " + userAge + " years old. Good luck as you " + userQuest + "!");
---
+++ winterscripts.js
@@ -0,0 +1,31 @@
+var response = prompt('What is the air-speed velocity of an unladen swallow?');
+
+if (response==='African or European?') {
+  prompt('You may pass.');
+} else {
+  prompt('You shall not pass.')
+};
+
+/*var response = prompt('What is the air-speed velocity of an unladen swallow?');
+
+if (response==='African or European?') {
+  prompt('You may pass.');
+} else {
+  prompt('You shall not pass.')
+}; */
+
+var quizA1 = prompt("What is Lily Potter's maiden name?");
+
+if (quizA1==='Evans') {
+  alert('Ten points to Ravenclaw!')
+} else {
+  alert('Detention, Potter!')
+};
+
+var quizA2 = prompt("How many dragons does Danerys have?");
+
+if (quizA2==='3') {
+  alert('Ten points to Ravenclaw!')
+} else {
+  alert('Detention, Potter!')
+};
